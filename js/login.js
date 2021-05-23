6be84ec7e02d4b0e7c3becf29dbbe9a02c1e5ce5
KinOnik <84587417+KinOnik@users.noreply.github.com>
2021-05-23 21:21:04 +0300

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,14 +1,24 @@
 var pass = document.querySelector('.vv_pas');
 var login = document.querySelector('.vv_log');
-
+var but = document.querySelector('button');
 
 pass.addEventListener('input', changeBackground);
 login.addEventListener('input', changeBackground);
+but.addEventListener('click', pustite);
+	
 
 function changeBackground() {
   if (pass.value !== '' && login.value !== '') {
-    document.querySelector('button').style.backgroundImage  = "url('img/logIn_1.png')"
+    document.querySelector('button').style.backgroundImage  = "url('img/logIn_1.png')"    
   } else {
     document.querySelector('button').style.backgroundImage  = "url('img/logIn_2.png')";
+  }
+}
+
+function pustite(){
+	if (pass.value == '1234' && login.value == 'KinOn') {
+  	document.location.href ="news.html";
+  } else {
+  	alert('Логин или Пароль введены не верно!');
   }
 }
